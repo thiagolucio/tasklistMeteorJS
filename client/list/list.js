47e2186f4b96c5dcbb1060ff2c9685be70ba08cb
thiagolucio <thiagolucio@gmail.com>
2018-03-23 20:16:25 -0300

    push the first component to work with project

--- client/list/list.js
+++ client/list/list.js
@@ -1,3 +1,14 @@
-Template.list.viewmodel({
-    
+Template.list.helpers({
+    //create a mock tasks
+    tasks: function () {
+        return [
+            { name: "First task from list" },
+            { name: "Second task from list" },
+            { name: "Fourth task from list" },
+            { name: "Fifth task from list" }
+        ];
+    }
 });
+// Template.list.viewmodel({
+    
+// });
